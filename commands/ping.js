30e443a6ee1c3a3da5576fbeee9cca274fb223a9
fantong <fankaihsiang11@gmail.com>
2021-01-31 01:38:46 +0800

    Add args detect and cooldown.

--- commands/ping.js
+++ commands/ping.js
@@ -1,8 +1,10 @@
 module.exports = {
     name: "ping",
     description: "Ping!",
+    args: false,
     execute(message, args) {
         const timeTaken = Date.now() - message.createdTimestamp;
+        //message.reply(Date.now() + " " + message.createdTimestamp);
         message.reply(`${timeTaken}ms`);
     }
 }
--- index.js
+++ index.js
@@ -29,8 +29,7 @@ client.once("disconnect", () => {
 });
 
 client.on("message", async (message) => {
-    if (message.author.bot) return;
-    if (!message.content.startsWith(prefix)) return;
+    if (!message.content.startsWith(prefix) || message.author.bot) return;
 
     // 拿掉前面prefix的長度
     const commandBody = message.content.slice(prefix.length);
@@ -39,7 +38,42 @@ client.on("message", async (message) => {
     // 陣列左移再變小寫
     const commandName = args.shift().toLowerCase();
 
+    if (!client.commands.has(commandName)) return;
+
     const command = client.commands.get(commandName);
+
+    if (command.args && !args.length) {
+        let reply = (`You didn't provide and arguments, ${message.author}!`);
+
+        if (command.usage) {
+            reply += `\nThe proper usage would be: \`${prefix}${command.name} ${command.usage}\``;
+        }
+
+        return message.channel.send(reply);
+    }
+
+    const cooldowns = new Discord.Collection();
+
+    if (!cooldowns.has(command.name)) {
+        cooldowns.set(command.name, new Discord.Collection());
+    }
+
+    const now = Date.now();
+    const timestamps = cooldowns.get(command.name);
+    const cooldownAmount = (command.cooldown || 3) * 1000;
+
+    if (timestamps.has(message.author.id)) {
+        const expirationTime = timestamps.get(message.author.id) + cooldownAmount;
+
+        if (now < expirationTime) {
+            const timeLeft = (expirationTime - now) / 1000;
+            return message.reply(`please wait ${timeLeft.toFixed(1)} more second(s) before reusing the \`${command.name}\` command.`)
+        }
+    }
+
+    timestamps.set(message.author.id, now);
+    setTimeout(() => timestamps.delete(message.author.id), cooldownAmount);
+
     try {
         command.execute(message, args);
     }
