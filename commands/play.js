6fd1b3110e992c92b89f4ce6e22bf3bfeaa3871c
fantong <fankaihsiang11@gmail.com>
2021-01-31 01:38:46 +0800

    Add args detect and cooldown.

--- commands/play.js
+++ commands/play.js
@@ -3,7 +3,10 @@ const ytdl = require('ytdl-core');
 module.exports = {
     name: "play",
     description: "Play music",
-    async execute(message) {
+    args: true,
+    usage: "<YouTube Link>",
+    cooldowns: 5,
+    async execute(message, args) {
         if (message.member.voice.channel) {
             const connection = await message.member.voice.channel.join();
             //connection.play("../01. Realize.mp3");
