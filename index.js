88932dcc076cb7d22fc9177f035726406cb3544a
107590051 <t107590051@ntut.org.tw>
2022-04-09 21:19:56 +0800

    deploy

--- index.js
+++ index.js
@@ -3,23 +3,23 @@ const bodyParser = require("body-parser");
 const DiscordBot = require("./app/helpers/DiscordBot");
 var cors = require("cors");
 
-const server = "https://fantong-email-tracker.herokuapp.com/";
-const app = express();
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-app.use(cors());
+// const server = "https://fantong-discord-bot.herokuapp.com/";
+// const app = express();
+// app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.json());
+// app.use(cors());
 
-app.get("/", (req, res) => {
-    res.json({
-        "message": "Hi this is mail tracker node server",
-        "url": server
-    });
-});
+// app.get("/", (req, res) => {
+//     res.json({
+//         "message": "Hi this is mail tracker node server",
+//         "url": server
+//     });
+// });
 
-app.use("/api", require("./app/routes/EmailRoute.js"));
+// app.use("/api", require("./app/routes/EmailRoute.js"));
 
-app.listen(process.env.PORT || 5000, () => {
-    console.log("Server is listening...url: " + server);
-});
+// app.listen(process.env.PORT || 5000, () => {
+//     console.log("Server is listening...url: " + server);
+// });
 
 new DiscordBot().run();
