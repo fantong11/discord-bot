df99e866926c339389dcff9e663f2681e68fcd84
107590051 <t107590051@ntut.org.tw>
2022-04-09 22:09:05 +0800

    deploy

--- app/services/FirebaseService.js
+++ app/services/FirebaseService.js
@@ -2,18 +2,18 @@ const { initializeApp } = require("firebase/app");
 const { getFirestore } = require("firebase/firestore/lite");
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBfl8oVNNZhgd369V2xuXeQDZuf0gd5A80",
-  authDomain: "email-tracker-1b98e.firebaseapp.com",
-  projectId: "email-tracker-1b98e",
-  storageBucket: "email-tracker-1b98e.appspot.com",
-  messagingSenderId: "283614079692",
-  appId: "1:283614079692:web:4311e6e31917ad0676b069"
+	apiKey: process.env.FIREBASE_API_KEY,
+	authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+	projectId: process.env.FIREBASE_PROJECT_ID,
+	storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+	messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+	appId: process.env.FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
 module.exports = {
-  app: app,
-  db: db
+	app: app,
+	db: db
 };
