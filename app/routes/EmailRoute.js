ee4c69d5b0f86dcfbedcacf27c6db6c136002e01
Fantong <fankaihsiang11@gmail.com>
2026-03-05 22:55:24 +0800

    refactor: remove unused dependencies from package.json

--- app/routes/EmailRoute.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const email = require("../controllers/EmailController");
-
-
-router.get("/recipients/:recipient", email.detectEmailOpen);
-router.post("/sendmail", email.sendMail);
-
-module.exports = router;
